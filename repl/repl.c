517f51e7c3a51124f9051df8e5812c49c233bd86
Gabriel Malaquias <5194287+GabrielMalakias@users.noreply.github.com>
2021-03-14 14:47:22 +0100

    Evaluate S-Expressions

--- repl/repl.c
+++ repl/repl.c
@@ -163,46 +163,93 @@ int min(int x, int y) {
   return (x > y) ? y : x;
 }
 
-/*
-lval eval_op(lval x, char* operator, lval y) {
-  if (x.type == LVAL_ERR) { return x; }
-  if (y.type == LVAL_ERR) { return y; }
+lval* lval_eval_sexpr(lval* v);
 
-  if (strcmp(operator, "+") == 0) { return lval_num(x.num + y.num); }
-  if (strcmp(operator, "-") == 0) { return lval_num(x.num - y.num); }
-  if (strcmp(operator, "*") == 0) { return lval_num(x.num * y.num); }
-  if (strcmp(operator, "%") == 0) { return lval_num(x.num % y.num); }
-  if (strcmp(operator, "^") == 0) { return lval_num(pow(x.num, y.num)); }
-  if (strcmp(operator, "min") == 0) { return lval_num(min(x.num, y.num)); }
-  if (strcmp(operator, "max") == 0) { return lval_num(max(x.num, y.num)); }
-  if (strcmp(operator, "/") == 0) {
-    return y.num == 0
-      ? lval_err(LERR_DIV_ZERO)
-      : lval_num(x.num / y.num);
-  }
-  return lval_err(LERR_BAD_OP);
+lval* lval_eval(lval* v) {
+  if(v->type == LVAL_SEXPR) { return lval_eval_sexpr(v); }
+
+  return v;
 }
 
-lval eval(mpc_ast_t* ast) {
-  if (strstr(ast->tag, "number")) {
-    errno = 0;
-    long x = strtol(ast->contents, NULL, 10);
-    return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
+lval* lval_pop(lval* v, int i) {
+  lval* x = v->cell[i];
+
+  memmove(&v->cell[i], &v->cell[i+1], sizeof(lval*) * (v->count-i-1));
+
+  v->count--;
+
+  v->cell = realloc(v->cell, sizeof(lval*) * v->count);
+
+  return x;
+}
+
+lval* lval_take(lval* v, int i) {
+  lval* x = lval_pop(v, i);
+  lval_del(v);
+  return x;
+}
+
+/* Manages the stack */
+lval* builtin_op(lval* a, char* operator) {
+  for(int i = 0; i < a->count; i++) {
+    if(a->cell[i]->type != LVAL_NUM) {
+      lval_del(a);
+      return lval_err("Cannot operate on non-number!");
+    }
   }
 
-  char* operator = ast->children[1]->contents;
+  lval* x = lval_pop(a, 0);
 
-  lval result = eval(ast->children[2]);
-
-  int index = 3;
-  while(strstr(ast->children[index]->tag, "expr")) {
-    result = eval_op(result, operator, eval(ast->children[index]));
-    index++;
+  if((strcmp(operator, "-") == 0) && a->count == 0) {
+    x->num = -x->num;
   }
+
+  while(a->count > 0) {
+    lval* y = lval_pop(a, 0);
+
+    if (strcmp(operator, "+") == 0) { x->num += y->num; }
+    if (strcmp(operator, "-") == 0) { x->num -= y->num; }
+    if (strcmp(operator, "*") == 0) { x->num *= y->num; }
+    if (strcmp(operator, "%") == 0) { x->num %= y->num; }
+    if (strcmp(operator, "^") == 0) { x->num ^= y->num; }
+    if (strcmp(operator, "/") == 0) {
+      if(y->num == 0) {
+        lval_del(x); lval_del(y);
+        x = lval_err("Division by Zero!"); break;
+      }
+      x->num /= y->num;
+    }
+
+    lval_del(y);
+  }
+
+  lval_del(a); return x;
+}
+
+lval* lval_eval_sexpr(lval* v) {
+  for(int i = 0; i < v->count; i++) {
+    v->cell[i] = lval_eval(v->cell[i]);
+  }
+
+  for(int i = 0; i < v->count; i++) {
+    if(v->cell[i]->type == LVAL_ERR) { return lval_take(v, i); }
+  }
+
+  if(v->count == 0) { return v; }
+
+  if(v->count == 1) { return lval_take(v, 0); }
+
+  lval* f = lval_pop(v, 0);
+  if(f->type != LVAL_SYM) {
+    lval_del(f); lval_del(v);
+    return lval_err("S-Expression does not start with symbol!");
+  }
+
+  lval* result = builtin_op(v, f->sym);
+  lval_del(f);
 
   return result;
 }
-*/
 
 int main(int argc, char** argv) {
   mpc_parser_t* Number     = mpc_new("number");
@@ -231,9 +278,9 @@ int main(int argc, char** argv) {
 
     mpc_result_t result;
     if(mpc_parse("<stdin>", input, Cisp, &result)) {
-      lval* ast = lval_read(result.output);
-      lval_println(ast);
-      lval_del(ast);
+      lval* x = lval_eval(lval_read(result.output));
+      lval_println(x);
+      lval_del(x);
 
       mpc_ast_delete(result.output);
     } else {
